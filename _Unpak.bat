0a3261e06c117939893fa0c18bb7aec4fac694e7
faceless-op <kaushikpalit2020@gmail.com>
2025-09-03 20:57:21 -0800

    Initial commit with large files tracked via Git LFS

---
+++ _Unpak.bat
@@ -0,0 +1,55 @@
+@echo off
+cls
+tecuha.exe x -o+ -vm+ -y+ tecX.uha
+del tecX.uha
+cls
+tecuha.exe x -o+ -vm+ -y+ tecjpg.uha
+del tecjpg.uha
+cls
+tecuha.exe x -o+ -vm+ -y+ tecmisc.uha
+del tecmisc.uha
+cls
+tecuha.exe x -o+ -vm+ -y+ tecbsp.uha
+del tecbsp.uha
+cls
+tecuha.exe x -o+ -vm+ -y+ tecdds.uha
+del tecdds.uha
+cls
+tecuha.exe x -o+ -vm+ -y+ tecgsc.uha
+del tecgsc.uha
+cls
+tecuha.exe x -o+ -vm+ -y+ tecmap.uha
+del tecmap.uha
+cls
+tecuha.exe x -o+ -vm+ -y+ tecogg.uha
+del tecogg.uha
+cls
+tecuha.exe x -o+ -vm+ -y+ tecmain.uha
+del tecmain.uha
+del tecuha.exe
+cls
+call TecDuper.bat
+del TecDuper.bat
+cls
+call TecAudio.bat
+del TecAudio.bat
+cls
+call TeConvert.bat
+del TeConvert.bat
+cls
+tecsetup.exe
+cls
+echo      #######################################################
+echo      ##OK, all done. Enjoy another Fine TECHNIC release !!##
+echo  #   #######################################################
+echo      #                                                     #
+echo    #   _ __•••••••• •••  •• •  •  •• •• ••  •••            #
+echo      _   _  _  ••• ••  ••  ••  •• ••••   • ••              #
+echo # _     _   __ ••• ••• ••  •••••• • ••   • ••              #
+echo   _   _   _  _ ••• ••  ••  ••  •• •  •   • ••              #
+echo _       _   _   ••• ••• ••• •  •  ••  •• •• •••••          #
+echo                                                            #
+echo    # #######################################################
+echo   #  ##OK, all done. Enjoy another Fine TECHNIC release !!##
+echo   #  #######################################################
+pause
